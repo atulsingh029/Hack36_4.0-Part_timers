666fb856b8482987b1acea7e67d54d2cf4b753bc
atul_singh <atulsingh29@protonmail.com>
2021-04-10 10:29:24 +0530

    model and admin config added

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+@admin.register(Account)
+class Account(UserAdmin):
+    list_display = ['username','id','email','is_staff','first_name']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('User Type'), {'fields': ('type', 'badge','badge_title')}),
+        (('Extra Profile Builder'), {'fields': ('sex','dob','profile_pic','phone', 'other_info')}),
+        (('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(Story)
+admin.site.register(EmergencyContact)
+admin.site.register(AlertContact)
+admin.site.register(Comment)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(User):
+    sex = models.CharField(max_length=15, choices=(('male', 'male'), ('female', 'female')), null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='profile_pictures', default='def_user.png')
+    phone = models.PositiveIntegerField(null=True, blank=True)
+    type = models.CharField(choices=[('basic', 'basic'), ('advance', 'advance')], max_length=10)
+    badge = models.CharField(choices=[('volunteer', 'volunteer'), ('victim', 'victim')], max_length=10)
+    badge_title = models.CharField(max_length=50, null=True, blank=True)
+    other_info = models.CharField(max_length=4048, null=True, blank=True)
+
+
+class AlertContact(models.Model):
+    name = models.CharField(max_length=255)
+    geo_code = models.CharField(max_length=255)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField(max_length=1024)
+    contact_type = models.CharField(max_length=50, choices=(('police_station', 'police_station'),
+                                                            ('women_helpline', 'women_helpline'), ('other', 'other')))
+
+    def __str__(self):
+        return self.geo_code
+
+
+class EmergencyContact(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField(max_length=1024)
+
+
+class Voice(models.Model):
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+
+
+class Story(models.Model):
+    ip = models.GenericIPAddressField()
+    datetime = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    storyid = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=512, null=False, blank=False)
+    story = models.CharField(max_length=2048, null=False, blank=False)
+    writer = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='story')
+    voice = models.ManyToManyField(Voice, blank=True)
+    status = models.CharField(choices=[('requested', 'requested'), ('accepted', 'accepted'), ('rejected', 'rejected')],
+                              max_length=10, default='requested')
+    visibility = models.CharField(
+        choices=[('public', 'public'), ('advance user only', 'advance user only'), ('private', 'private')],
+        max_length=20, default='public')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=512)
+    datetime = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    writer = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
